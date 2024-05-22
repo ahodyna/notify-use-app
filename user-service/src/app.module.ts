52fcf4e888852657eacde16cfdeaa8e78d8d34f0
alina <hodyna.alina@gmail.com>
2024-05-22 20:13:51 +0300

    remove knex; added typeorm

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksService } from './cron/message-service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      username: 'obrio_app',
+      password: 'obrio_app_password',
+      database: 'user_service',
+      synchronize: true,
+      host: 'localhost',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, TasksService],
 })
--- user-service/src/database/db.js
+++
@@ -1,5 +0,0 @@
-import knex from 'knex';
-import knexfile from '../../knexfile';
-
-const db = knex(knexfile.development);
-module.exports = db;
