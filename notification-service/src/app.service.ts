78f19eb7d948e958410361afa15cb04f5c4d53c8
alina <hodyna.alina@gmail.com>
2024-05-23 10:50:52 +0300

    connect notification-service to rabbitMQ

--- notification-service/src/app.service.ts
+++ notification-service/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  handlerNotification(data: any) {
+    console.log('Received data:', data);
   }
 }
